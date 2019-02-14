065f5797053f13af6cdd43964515ae0e75c495b8
Usuário Convidado <Guest@pa06micro24.local>
2019-02-13 23:28:59 -0200

    aula02

---
+++ MoviesLib/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 13/02/19.
+//  Copyright © 2019 FIAP. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let title: String
+    let categories: String
+    let duration: String
+    let rating: Double
+    let summary: String
+    let image: String
+    
+}
